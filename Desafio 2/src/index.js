df7c37df0359699a11b508216640f329a6ff2dc3
emtasa <emasanz08@gmail.com>
2022-09-22 16:49:30 -0300

    Desafio 2

---
+++ Desafio 2/src/index.js
@@ -0,0 +1,43 @@
+import Container from "./containers/Container.js";
+
+const productContainer = new Container("products");
+
+//------- Get All-------
+
+// productContainer
+//   .getAll()
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }));
+
+//-------Save-------
+
+// productContainer
+//   .save({
+//     title: "Manzana",
+//     price: 1000,
+//     thumbnail: "https://imgprueba.com.ar",
+//   })
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }));
+
+//-------Get By Id-------
+
+// productContainer
+//   .getById(4)
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }));
+//
+
+//-------Delete By Id-------
+
+// productContainer
+//   .deleteById(5)
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }));
+
+//-------Delete All-------
+
+// productContainer
+//   .deleteAll()
+//   .then((data) => console.log({ data }))
+//   .catch((error) => console.log({ error }));
