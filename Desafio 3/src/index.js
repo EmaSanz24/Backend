d829c07f23e8e8f9808f18f537ea33eb6650424e
emtasa <emasanz08@gmail.com>
2022-10-04 17:21:28 -0300

    Desafio 3

---
+++ Desafio 3/src/index.js
@@ -0,0 +1,22 @@
+import Container from "./containers/Container.js";
+import express from "express";
+
+const productContainer = new Container("products");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/products", (req, res) => {
+  productContainer.getAll().then((list) => {
+    res.send(list);
+  });
+});
+
+app.get("/randomProduct", (req, res) => {
+  productContainer
+    .getAll()
+    .then((parseList) => parseList[(Math.random() * parseList.length) | 0])
+    .then((itemList) => res.send(itemList));
+});
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
