2f815f54c7036c00178498abe744914687bcaa0a
emtasa <emasanz08@gmail.com>
2022-11-22 18:19:36 -0300

    Segunda Entrega

---
+++ PF_Segunda_Entrega/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { CartRouter, ProductRouter } from "./routers/index.js";
+import { config } from "./config/index.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", ProductRouter);
+app.use("/api/cart", CartRouter);
+
+const server = app.listen(config.SERVER.PORT, () => console.log(`Server running on port ${server.address().port}`));
---
+++ PF_Segunda_Entrega/src/services/index.js
@@ -0,0 +1,2 @@
+export { MongoDBService } from "./MongoDBService/index.js";
+export { FirebaseDBService } from "./FirebaseDBService/index.js";
